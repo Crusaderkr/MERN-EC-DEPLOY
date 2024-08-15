4bafe1954061497b264b64e2621bd1b12ac30071
Kapil Rawat <70755182+Crusaderkr@users.noreply.github.com>
2024-08-15 21:02:12 +0530

    Update Item.jsx

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import './Item.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Item.css';
+import { Link } from 'react-router-dom';
+
 const Item = (props) => {
+  // Assume images are hosted at a base URL
+  const imageUrl = `https://your-server-url.com/images/${props.image}`;
+
   return (
     <div className='item'>
-        <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)}src={props.image} alt="not avilable" /></Link>
-        <p>{props.name}</p>
-        <div className="item-prices">
-            <div className="item-price-new">
-            &#8377;{props.new_price}
-            </div>
+      <Link to={`/product/${props.id}`}>
+        <img
+          onClick={() => window.scrollTo(0, 0)}
+          src={imageUrl}
+          alt="Image not available"
+        />
+      </Link>
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">
+          &#8377;{props.new_price}
+        </div>
         <div className="item-price-old">
-            {props.old_price}
+          {props.old_price}
         </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Item
+export default Item;
