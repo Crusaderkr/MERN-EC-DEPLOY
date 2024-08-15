b42f4a9120b8a03df86b9b818f903bdf378258c4
Kapil Rawat <70755182+Crusaderkr@users.noreply.github.com>
2024-08-15 11:23:32 +0530

    Update NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import './NewCollections.css'
 import Item from '../Item/Item'
+const url ='https://mern-ec-deploy-backend.onrender.com';
 const NewCollections = () => {
 
   const [new_collection,setNew_collection] = useState([]);
 
 useEffect(()=>{
-    fetch('http://localhost:4000/newcollections')
+    fetch(`${url}/newcollections`)
     .then((response)=>response.json())
     .then((data)=>setNew_collection(data));
 },[])
