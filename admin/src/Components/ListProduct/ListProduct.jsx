d7cbe87b4d95c0a86f0cf2f27752e45a2de3c3a3
Kapil Rawat <70755182+Crusaderkr@users.noreply.github.com>
2024-08-16 08:05:14 +0530

    Update ListProduct.jsx

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './ListProduct.css';
 import cross_icon from '../../assets/cross_icon.png';
 
-const url ='https://mern-ec-deploy-backend.onrender.com';
+const url = 'https://mern-ec-deploy-backend.onrender.com';
 const ListProduct = () => {
   const [allProducts, setAllProducts] = useState([]);
 
@@ -59,6 +59,7 @@ const ListProduct = () => {
           <React.Fragment key={product.id}>
             <div className="list-product-format">
               <div>
+                {/* Fixing the URL construction by adding the necessary slash */}
                 <img src={`${url}${product.image}`} alt="your image" className="listproduct-product-icon" />
               </div>
               <div>
@@ -91,4 +92,3 @@ const ListProduct = () => {
 };
 
 export default ListProduct;
-
