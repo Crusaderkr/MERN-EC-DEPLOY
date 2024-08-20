c8d051d177f1a43981fc71d7e0fa610530535494
Kapil Rawat <70755182+Crusaderkr@users.noreply.github.com>
2024-08-20 21:38:02 +0530

    Update RelatedProducts.jsx

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
-import './RelatedProducts.css'
-import data_product from '../Assets/data';
+import React, { useEffect, useState } from 'react';
+import './RelatedProducts.css';
 import Item from '../Item/Item';
 
 const RelatedProducts = () => {
+  const [relatedProducts, setRelatedProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchRelatedProducts = async () => {
+      try {
+        const res = await fetch('https://mern-ec-deploy-backend.onrender.com/relatedproducts');
+        const data = await res.json();
+        setRelatedProducts(data);
+      } catch (error) {
+        console.error('Error fetching related products:', error);
+      }
+    };
+
+    fetchRelatedProducts();
+  }, []);
+
   return (
     <div className='relatedproducts'>
         <h1>Related Products</h1>
         <hr />
         <div className="relatedproducts-item">
-            {data_product.map((item,i)=>{
-                            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-
-            })}
+            {relatedProducts.map((item, i) => (
+                <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            ))}
         </div>
-      
     </div>
   )
 }
 
-export default RelatedProducts
+export default RelatedProducts;
