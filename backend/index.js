3e32a9c9b1abd3a47b29e5301ed197e43b08c807
Kapil Rawat <70755182+Crusaderkr@users.noreply.github.com>
2024-08-20 22:22:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -65,4 +65,4 @@ app.post('/addproduct', async (req, res) => {
 // Server setup
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log("Server running on port" + ${port});
+  console.log("Server running on port  ${port}");
